a77c73cbfa86423f131221e31b3859cea5a961b6
霍怡童 <huoyt@csdn.net>
2020-09-01 08:04:54 +0800

    dev: dev

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let http = require('http');
 let users = [
-    {id: 11, name: 'tong11'},
-    {id: 22, name: 'tong22'},
-    {id: 33, name: 'tong33'}
+    {id: 111, name: 'tong11'},
+    {id: 222, name: 'tong22'},
+    {id: 333, name: 'tong33'}
 ]
 let server = http.createServer(function(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
