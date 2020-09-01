0e20e17b0dbefe5030aa7e19e2a7295a2969e933
Geekhyt <hungryturbo@gmail.com>
2020-09-01 22:31:30 +0800

    dev: dev

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let users = [
     {id: 33, name: 'tong33'}
 ]
 let server = http.createServer(function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeaders('Access-Control-Allow-Origin', '*');
     if (req.url === '/api/users') {
         res.end(JSON.stringify(users));
     } else {
